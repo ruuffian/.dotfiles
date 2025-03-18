297e54df62c17a0f2f0e20f615c6929f4ab9d9d6
ruuffian <ruuffian@gmail.com>
2025-03-18 18:39:07 -0400

    Add tmux conf to setup.sh

--- setup.sh
+++ setup.sh
@@ -16,6 +16,7 @@ touch .bash_local
 touch ~/.bash_profile
 echo ". ~/.bashrc" >> ~/.bash_profile
 ln -s /home/ruuffian/code/tooling/.dotfiles/nvim-config ~/.config/nvim
+ln -s /home/ruuffian/code/tooling/.dotfiles/.tmux.conf ~/.tmux.conf
 ln -s /home/ruuffian/code/tooling/.dotfiles/.local/bin/tmux-sessionizer .local/bin/tmux-sessionizer
 # Git config
 git config --global user.email ruuffian@gmail.com
