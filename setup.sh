b446b99f6170a4c1de4255828b9e822f322680e1
ruuffian <ruuffian@gmail.com>
2025-06-10 02:51:16 -0400

    Added some config such as foot (terminal) and polybar (not working). Need to investigate more, might switch back to alacritty for better config. Also not super tied to polybar, waybar seems more than enough. bswpm scares me but interests me a lot.

--- setup.sh
+++ setup.sh
@@ -12,12 +12,14 @@ git submodule update --init --recursive
 # Symbolic links
 ln -s /home/ruuffian/code/tooling/.dotfiles/.bashrc ~/.bashrc
 ln -s /home/ruuffian/code/tooling/.dotfiles/.bash_aliases ~/.bash_aliases
+ln -s /home/ruuffian/code/tooling/.dotfiles/.config/foot ~/.config/foot
+ln -s /home/ruuffian/code/tooling/.dotfiles/.config/plasma-workspace ~/.config/plasma-workspace
 touch .bash_local
 touch ~/.bash_profile
 echo ". ~/.bashrc" >> ~/.bash_profile
 ln -s /home/ruuffian/code/tooling/.dotfiles/nvim-config ~/.config/nvim
 ln -s /home/ruuffian/code/tooling/.dotfiles/.tmux.conf ~/.tmux.conf
-ln -s /home/ruuffian/code/tooling/.dotfiles/.local/bin/tmux-sessionizer .local/bin/tmux-sessionizer
+ln -s /home/ruuffian/code/tooling/.dotfiles/.local/bin/tmux-sessionizer ~/.local/bin/tmux-sessionizer
 # Git config
 git config --global user.email ruuffian@gmail.com
 git config --global user.name ruuffian
